9fe877b20d1568326b0d4a4f12931d6135269975
Andrei004 <131450174+Andrei004@users.noreply.github.com>
2023-05-26 12:57:21 +0200

    cambio rama1

--- Andrei_Daniliuc_ExamenGit2/src/pkg/Clase.java
+++ Andrei_Daniliuc_ExamenGit2/src/pkg/Clase.java
@@ -5,6 +5,6 @@ public class Clase {
 	public String param3="param2";
 	public String param4="param3";
 	public String param5="param4";
-	public String param5="param6";
 	public String param6="param5";
+	public String param7="param6";
 }
